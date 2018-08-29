b04b61b48eb57f2ac314466d8dba6fccfafec0c3
Jeffrey Rios <yefriddavid@gmail.com>
2018-08-28 23:24:16 -0300

    s

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
     "net/http"
 )
 
+var arrWs [2]websocket.Conn
+
 func Echo(ws *websocket.Conn) {
     var err error
-
     for {
         var reply string
 
@@ -20,10 +21,25 @@ func Echo(ws *websocket.Conn) {
 
         fmt.Println("Received back from client: " + reply)
 
-        msg := "Received:  " + reply
-        fmt.Println("Sending to client: " + msg)
+        var msg string = "Ask me something!!"
+	if reply == "where are you from!!" {
+		msg = "That is not important"
+	}
+	if reply == "How old are you" {
+		msg = "I am 29 years old"
+	}
+
+        if arrWs[1] == range websocket {
+		arrWs[1] = ws
+	}
+        //fmt.Println("Sending to client: " + msg)
 
         if err = websocket.Message.Send(ws, msg); err != nil {
+            fmt.Println("Can't send")
+            break
+        }
+
+        if err = websocket.Message.Send(ws[1], msg); err != nil {
             fmt.Println("Can't send")
             break
         }
@@ -31,9 +47,9 @@ func Echo(ws *websocket.Conn) {
 }
 
 func main() {
-    http.Handle("/socketxxx", websocket.Handler(Echo))
+    http.Handle("/sock", websocket.Handler(Echo))
 
-    if err := http.ListenAndServe(":1234", nil); err != nil {
+    if err := http.ListenAndServe(":1588", nil); err != nil {
         log.Fatal("ListenAndServe:", err)
     }
 }
